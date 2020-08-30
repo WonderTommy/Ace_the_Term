eadc7197dac6e4acf69f7ce1ded4eb4914d414e4
Hao Li <hlifor931@gmail.com>
2020-08-30 11:37:11 -0400

    Initial Commit

---
+++ GradeCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradeCalculator
+//
+//  Created by Hao Li on 2020-08-30.
+//  Copyright © 2020 Hao Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
